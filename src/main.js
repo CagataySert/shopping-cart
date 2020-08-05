406a546675d8ce336ac7183ad9ebdc635c99286b
cagataysert <cagatay.sert5@gmail.com>
2020-08-06 01:49:28 +0300

    mock data producer

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const { discountTypes } = require("./constants");
+const Category = require("./category");
+const Product = require("./product");
+const Campaign = require("./campaign");
+const Coupon = require("./coupon");
+const Cart = require("./shopping-cart");
+
+const main = () => {
+  // #region category start
+  const foodCategory = new Category("food");
+  const electronicCategory = new Category("electronic");
+  // #endregion category finish
+
+  // #region product start
+  const orange = new Product("Orange", 20, foodCategory);
+  const apple = new Product("Apple", 60, foodCategory);
+
+  const keyboard = new Product("Keyboard", 350, electronicCategory);
+  const mouse = new Product("Mouse", 250, electronicCategory);
+  // #endregion product finish
+
+  // #region campaigns start
+  const firstCampaign = new Campaign(foodCategory, 30, 30, discountTypes.rate);
+  const secondCampaign = new Campaign(
+    electronicCategory,
+    10,
+    5,
+    discountTypes.rate
+  );
+  const thirdCampaign = new Campaign(foodCategory, 5, 10, discountTypes.amount);
+  // #endregion campaigns finish
+
+  //#region coupon start
+  const firstCoupon = new Coupon(100, 10, discountTypes.rate);
+  const secondCoupon = new Coupon(75, 25, discountTypes.amount);
+  //#endregion coupon finish
+
+  const cart = new Cart();
+
+  cart.addItem(orange, 30);
+  cart.addItem(apple, 20);
+  cart.addItem(keyboard, 5);
+  cart.addItem(mouse, 4);
+
+  cart.applyDiscounts(firstCampaign, secondCampaign, thirdCampaign);
+  cart.applyCoupon(firstCoupon, secondCoupon);
+  console.log(cart.detailInfo);
+};
+
+main();
